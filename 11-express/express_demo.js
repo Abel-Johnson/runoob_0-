b8abd42bd38b2571429f03d381e406f9dc6a736f
Abel-Johnson <734157604@qq.com>
2017-03-10 22:28:51 +0800

    -

--- 11-express/express_demo.js
+++ 11-express/express_demo.js
@@ -1,16 +1,30 @@
 var express = require('express');
-
 var app = express();
 
-//处理get请求
+//处理get请求(路由)
 app.get('/',function(req,res) {
-    res.send('HELLO WORLD');
+    res.send('HELLO get主页');
+});
+app.post('/',function(req,res) {
+    res.send('HELLO post主页')
+});
+app.get('/my*resume',function(req,res) {
+    res.send('我的简历')
 })
+app.get('/my*works',function(req,res) {
+    res.send('我的作品')
+})
+app.get('/resume',function(req,res) {
+    res.send('我的简历')
+})
+
 
 //搭建服务器
 var server = app.listen(8081, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log(host,port);
-    console.log('实例,访问地址为http://%s:%s',host,port);
+
+    // var host = server.address().address;
+    // var port = server.address().port;
+    // console.log(host,port);
+    // console.log('实例,访问地址为http://%s:%s',host,port);
+
 })
